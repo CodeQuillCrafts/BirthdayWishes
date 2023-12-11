8102cd188395d1c829f489bbe0e9b826223e2dd7
Shivam Gupta <codequillcrafts@gmail.com>
2023-12-11 17:01:09 +0530

    adding files

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-const root = document.querySelector('#root');
-const text = document.querySelector('#text');
-const age = 19;
-const birthdayAudio = new Audio('birthday.mp3');
+document.addEventListener('DOMContentLoaded', function () {
+    const root = document.querySelector('#root');
+    const text = document.querySelector('#text');
+    const age = 19;
+    const birthdayAudio = new Audio('birthday.mp3');
+    birthdayAudio.autoplay = true;
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 40) + 1;
-}
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 70) + 1;
+    }
 
-function addBalloon() {
-    let balloon = document.createElement('div');
-    balloon.textContent = '🎈';
-    balloon.classList.add('balloon');
-    const randomPadding = getRandomNumber() + 'px';
-    balloon.style.padding = randomPadding;
-    const randomMargin = getRandomNumber() + 'px';
-    balloon.style.margin = randomMargin;
-    root.appendChild(balloon);
-}
+    function addBalloon() {
+        let balloon = document.createElement('div');
+        balloon.textContent = '🎈';
+        balloon.classList.add('balloon');
+        const randomPadding = getRandomNumber() + 'px';
+        balloon.style.padding = randomPadding;
+        const randomMargin = getRandomNumber() + 'px';
+        balloon.style.margin = randomMargin;
+        root.appendChild(balloon);
+    }
 
-function displayAge(i) {
-    if (i <= age + 1) {
-        setTimeout(() => {
-            text.textContent = i + " years";
-            displayAge(i + 1);
-        }, 100);
-    } else {
-        text.textContent = "Happy Birthday";
-        text.style.color = "blue";
-        for (let index = 0; index < 2000; index++) {
-            addBalloon();
-        }
+    function playAudio() {
+        return new Promise((resolve, reject) => {
+            birthdayAudio.addEventListener('canplaythrough', function () {
+                console.log("audio start");
+                birthdayAudio.play();
+                console.log("audio end");
+                resolve();
+            });
 
-        document.addEventListener('mouseover', function () {
-            birthdayAudio.play();
+            birthdayAudio.addEventListener('error', function (error) {
+                reject(error);
+            });
         });
     }
-}
 
-displayAge(0);
+    async function displayAge(i) {
+        try {
+            if (i <= age + 1) {
+                await new Promise(resolve => setTimeout(resolve, 100));
+                text.textContent = i + " years";
+                await displayAge(i + 1);
+            } else {
+                text.textContent = "Happy Birthday";
+                text.style.color = "blue";
+                for (let index = 0; index < 2000; index++) {
+                    addBalloon();
+                }
+                await playAudio();
+            }
+        } catch (error) {
+            console.error('Error playing audio:', error);
+        }
+    }
+
+    displayAge(0);
+});
