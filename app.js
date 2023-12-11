67dc9b28c3cc21a519c52701db7ce5a99ded88c2
Shivam Gupta <codequillcrafts@gmail.com>
2023-12-11 10:35:55 +0530

    adding birthday files

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.querySelector('body');
+    const root = document.querySelector('#root');
+    const age = 19;
+
+    function displayAge(i) {
+        if (i <= age + 1) {
+            setTimeout(() => {
+                root.textContent = i + " years";
+                displayAge(i + 1);
+            }, 100);
+        } else {
+            root.textContent = "Happy Birthday";
+            root.style.color = "blue";
+        }
+    }
+    setTimeout(() => {
+
+        displayAge(0);
+    }, 2000);
+});
